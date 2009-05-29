35aa8d5c010455b247e7b43a1c0d8358d9b0089f
Benny <bdotdub@gmail.com>
2009-05-29 09:25:43 -0700

    added dynamic creation of objects

--- lib/gluestick/object.rb
+++ lib/gluestick/object.rb
@@ -1,5 +1,81 @@
 module Gluestick
   class Object
-    attr_reader :objectKey, :name    
+    private_class_method  :new
+    attr_reader           :objectKey, :title, :image, :link, :type    
+
+    def self.from_object(response)
+      raise TypeError if not response.instance_of?(Gluestick::AdaptiveBlueResponse)
+
+      object_xml  = response.response['object']
+      object      = nil
+
+	    @@categories = {
+	      :books               => Gluestick::BookObject,
+	      :electronics         => Gluestick::ElectronicObject,
+	      :movie_stars         => Gluestick::MovieStarObject,
+	      :movies              => Gluestick::MovieObject,
+	      :music               => Gluestick::MusicObject,
+	      :recording_artists   => Gluestick::RecordingArtistObject,
+	      :restaurants         => Gluestick::RestaurantObject,
+	      :stocks              => Gluestick::StockObject,
+	      :tv_shows            => Gluestick::TVShowObject,
+	      :video_games         => Gluestick::VideoGameObject,
+	      :wines               => Gluestick::WineObject
+	    }
+
+      if @@categories.has_key?(object_xml['type'].to_sym)
+        object = @@categories[object_xml['type'].to_sym].create
+      else
+        create
+      end
+
+      response.response['object'].each do |property, value|
+        object.instance_variable_set(:"@#{property.to_s}", value)
+      end
+
+      object
+    end
+
+    protected
+
+    def self.create
+      new
+    end
   end
+
+  class BookObject < Object
+  end
+
+  class ElectronicObject < Object
+  end
+
+  class MovieStarObject < Object
+  end
+
+  class MovieObject < Object
+    attr_reader :director, :year, :starring
+  end
+
+  class MusicObject < Object
+  end
+
+  class RecordingArtistObject < Object
+  end
+
+  class RestaurantObject < Object
+  end
+
+  class StockObject < Object
+  end
+
+  class TVShowObject < Object
+  end
+
+  class VideoGameObject < Object
+  end
+
+  class WineObject < Object
+  end
+
+
 end
--- test/gluestick/object_test.rb
+++ test/gluestick/object_test.rb
@@ -2,9 +2,50 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class ObjectTest < Test::Unit::TestCase
 
-  should "should respond to the user attributes" do
-    Gluestick::Object.new.should respond_to(:objectKey, :type)
+  def mock_object_from_object
+    stub_get("/object/get?objectId=mock_object", "object/get.xml")
+    response = Gluestick::Client.instance.get("/object/get",
+                                              :query => { :objectId => 'mock_object' })
+    Gluestick::Object.from_object(response)
   end
+
+  should "not be able to instantiate an interaction" do
+    lambda { Gluestick::Object.new }.should raise_error
+  end
+
+  should "be able to generate objects from factories" do
+    stub_get("/object/get?objectId=mock_object", "object/get.xml")
+    response = Gluestick::Client.instance.get("/object/get",
+                                              :query => { :objectId => 'mock_object' })
+    Gluestick::Object.from_object(response).should be_kind_of(Gluestick::Object)
+  end
+
+  should "should respond to the user attributes" do
+    @mock_object = mock_object_from_object
+    @mock_object.should respond_to( :objectKey,
+                                    :title,
+                                    :image, 
+                                    :link, 
+                                    :type)
+  end
+
+  context "object is a movie object" do
+    setup do
+      @movie_object = mock_object_from_object
+    end
+
+    should "be an instance of MovieObject and a kind of Object" do
+      @movie_object.should be_kind_of(Gluestick::Object)
+      @movie_object.should be_instance_of(Gluestick::MovieObject)
+    end
+
+    should "be able to access director" do
+      @movie_object.should respond_to(:director)
+      @movie_object.director.should == "Danny Boyle"
+    end
+  end
+
+
 
 end
 
